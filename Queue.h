a2fe9d694e9eb1bca4d5ac0a1418823a3b67a488
XXXBold <30773753+XXXBold@users.noreply.github.com>
2018-03-31 19:21:16 +0200

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,6 +1,7 @@
 #ifndef QUEUE_H_INCLUDED
   #define QUEUE_H_INCLUDED
 
+  #include <stdlib.h>
   #include "booltype.h"
 
   #define QUEUE_INCLUDE_THREADSAFETY /* Define to make this library Thread-Safe */
@@ -11,6 +12,23 @@ typedef struct TagQueue_t TagQueue;
 #define QUEUE_OPTION_SYNCHRONIZED 0x1
 
 /**
+ * Used if you add new Data to a Queue.
+ */
+typedef enum
+{
+  /**
+   * Will use the passed pointer directly and add it to the Queue.
+   * In this case, it must be allocated using pvQueue_AllocateElement_g.
+   */
+  eQueue_DataUseDirectly,
+  /**
+   * Will allocate a new Buffer for pvData, and copy the data there.
+   * Use this for all Data not allocated by pvQueue_AllocateElement_g.
+   */
+  eQueue_DataUseCopy
+}EQueue_DataInfo;
+
+/**
  * Creates a New Queue
  *
  * @param uiOptions _IN_ Options for the Queue, 0 for default.
@@ -19,23 +37,22 @@ typedef struct TagQueue_t TagQueue;
  */
 TagQueue *ptagQueue_New_g(unsigned int uiOptions);
 
+void *pvQueue_AllocateElement_g(size_t sSize);
+
 /**
  * Pushes an Item to the Queue
  *
  * @param ptagQueue _IN_ Queue to push the item to
  * @param pvData    _IN_ The item to push
- * @param iDataSize _IN_ Size of the item, in bytes
- * @param bCopyDataToNewBuffer
- *                  _IN_ true to allocate a new buffer for the data, <br>
- *                  false to store the Pointer of pvData directly.
- *                  In this case, pvData MUST be dynamically allocated.
+ * @param sDataSize _IN_ Size of the item, in bytes
+ * @param eDataInfo Info how to handle pvData, see enum.
  *
  * @return true on success, else false
  */
 bool bQueue_Push_g(TagQueue *ptagQueue,
                    void *pvData,
-                   int iDataSize,
-                   bool bCopyDataToNewBuffer);
+                   size_t sDataSize,
+                   EQueue_DataInfo eDataInfo);
 
 /**
  * Returns the count of elements in the Queue
@@ -51,9 +68,9 @@ int iQueue_GetElementsCount_g(TagQueue *ptagQueue);
  *
  * @param ptagQueue _IN_ The Queue
  *
- * @return The Size of the next Element in Bytes or < 0 if failed
+ * @return The Size of the next Element in Bytes or 0 if no Element present
  */
-int iQueue_GetNextElementSize_g(TagQueue *ptagQueue);
+size_t sQueue_GetNextElementSize_g(TagQueue *ptagQueue);
 
 
 /**
@@ -69,40 +86,34 @@ bool bQueue_DeleteNextElement_g(TagQueue *ptagQueue,
                                 int *piItemsRemaining);
 
 /**
- * Pops the next item from the Queue and stores it in pvData.
- * if pvData is NULL, the data is just popped out and freed.
+ * Pops the next item from the Queue and stores it in ppvData.
  *
  * @param ptagQueue  _IN_ The Queue
- * @param ppvData    _IN_OUT_ Pointer to Pointer to void, will be filled with next available data.
- * @param piDataSize _IN_OUT_ Size of ppvData, will be set to the size of ppvData.
+ * @param ppvData    _OUT_ Pointer to Pointer to void, will be set to data. Free after use!
+ * @param psDataSize _IN_OUT_ Size of ppvData, will be set to the size of ppvData.
  * @param piItemsRemaining
- *                   _OUT_ Remaining Items in Queue, can be NULL if not needed.
- * @param bCopyData  _IN_ If true, data will be copied to ppvData, must be preallocated then.
- *                   Else ppvData will be set to the original pointer of the data, must be freed after use.
+ *                   _OUT_ Remaining Items in Queue, pass NULL if not needed.
  *
  * @return true on success, else false
  */
 bool bQueue_Pop_g(TagQueue *ptagQueue,
                   void **ppvData,
-                  int *piDataSize,
-                  int *piItemsRemaining,
-                  bool bCopyData);
+                  size_t *psDataSize,
+                  int *piItemsRemaining);
 
 /**
  * Allows to Peek the next element's data in the Queue, without removing it.
+ * Do not modify or free ppvData!
  *
  * @param ptagQueue  _IN_ The Queue
- * @param ppvData     _OUT_ Target pointer for the data, see bCopyData.
- * @param piDataSize _IN_OUT_ Pass size of pvData, will be set to the size of next element's data.
- * @param bCopyData  _IN_ true copies data to ppvData, if it's large enough, false will set it to the original Pointer.
- *                   Don't Modify then!
+ * @param ppvData    _OUT_ Pointer will be set to data.
+ * @param psDataSize _OUT_ Will be set to the size of ppvData.
  *
  * @return true on success, else false.
  */
 bool bQueue_Peek_g(TagQueue *ptagQueue,
-                   void **pvData,
-                   int *piDataSize,
-                   bool bCopyData);
+                   void **ppvData,
+                   size_t *psDataSize);
 
 
 /**
